3ca2b82d343e08b6fd3afca74b2d1b7f81093a1e
Isaac N Reis <isaacreisn@gmail.com>
2025-08-22 17:58:14 -0300

    criando filtro de status dos livros

--- src/pages/Livros.jsx
+++ src/pages/Livros.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "../assets/styles/Livros.css";
 import books from "../data/books";
 import BookItem from "../components/BookItem";
 
 const Livros = () => {
+  const [filter, setFilter] = useState("todos");
+
+  const filteredBooks = books.filter((book) => {
+    if (filter === "todos") {
+      return true;
+    }
+    return book.status === filter;
+  });
+
   return (
     <div className="livros-container">
       <h2>Nossa Corrida Literária</h2>
@@ -11,8 +20,42 @@ const Livros = () => {
         Aqui você encontrará todos os livros que já lemos e os que estão por
         vir. Prepare-se para a aventura!
       </p>
+      <div className="filter-buttons">
+        <button
+          className={
+            filter === "todos" ? "filter-button active" : "filter-button"
+          }
+          onClick={() => setFilter("todos")}
+        >
+          Todos
+        </button>
+        <button
+          className={
+            filter === "atual" ? "filter-button active" : "filter-button"
+          }
+          onClick={() => setFilter("atual")}
+        >
+          Atual
+        </button>
+        <button
+          className={
+            filter === "futuro" ? "filter-button active" : "filter-button"
+          }
+          onClick={() => setFilter("futuro")}
+        >
+          Futuro
+        </button>
+        <button
+          className={
+            filter === "passado" ? "filter-button active" : "filter-button"
+          }
+          onClick={() => setFilter("passado")}
+        >
+          Passado
+        </button>
+      </div>
       <div className="book-list">
-        {books.map((book) => (
+        {filteredBooks.map((book) => (
           <BookItem key={book.id} book={book} />
         ))}
       </div>
